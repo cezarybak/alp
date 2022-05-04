3e8bce4a5b44b2559b92560c67818fbc177157f2
cezarybak <cezary.bak0@gmail.com>
2022-05-05 00:07:49 +0200

    style: add style

---
+++ src/components/CompanyElement/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CompanyElementItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ElementContent = styled.span``;
+
+export const ElementButton = styled.button`
+  height: 20px;
+`;
