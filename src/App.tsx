c434da038310418789307eaf82ddd29da5768b3a
cezarybak <cezary.bak0@gmail.com>
2022-05-04 17:52:42 +0200

    feat: add logic and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { AppRoutes } from './routing';
+import { GlobalStyles } from './styles/gloabalStyle.css';
+import { theme } from './styles/them';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <AppRoutes />
+      </ThemeProvider>
     </BrowserRouter>
   );
 };
